87f0d0727bc9846493f096c735d73e9e92a39b71
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-10-03 15:35:06 -0400

    Start working on encapsulation

--- docassemble/GithubFeedbackForm/github_issue.py
+++ docassemble/GithubFeedbackForm/github_issue.py
@@ -7,9 +7,12 @@ from docassemble.base.util import log, get_config
 
 # Authentication for user filing issue (must have read/write access to
 # repository to add issue to)
+__all__ = ['valid_github_issue_config', 'make_github_issue']
 USERNAME = get_config('github issues',{}).get('username')
 TOKEN = get_config('github issues',{}).get('token')
 
+def valid_github_issue_config():
+  return bool(TOKEN)
 
 def make_github_issue(repo_owner, repo_name, template=None, title=None, body=None):
     """
@@ -18,6 +21,10 @@ def make_github_issue(repo_owner, repo_name, template=None, title=None, body=Non
     url = 'https://api.github.com/repos/%s/%s/issues' % (repo_owner, repo_name)
     # log(url)
     # Headers
+    if not TOKEN:
+      log("Error creating issues: No valid GitHub token provided.")
+      return None
+    
     headers = {
         "Authorization": "token %s" % TOKEN,
         "Accept": "application/vnd.github.v3+json"
@@ -38,4 +45,4 @@ def make_github_issue(repo_owner, repo_name, template=None, title=None, body=Non
     if response.status_code == 201:
         return response.json().get('html_url')
     else:
-        return 'Could not create Issue "%s", results %s' % (title, str(response.content))
+        log(f'Could not create Issue "{title}", results {response.content}')
--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.GithubFeedbackForm',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.MAVirtualCourt'],
+      install_requires=['docassemble.MAVirtualCourt>=1.0.22'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/GithubFeedbackForm/', package='docassemble.GithubFeedbackForm'),
      )
