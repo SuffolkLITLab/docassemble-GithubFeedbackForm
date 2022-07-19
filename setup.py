ce27461b177dfb820d21838638ab9286ebd3d8a3
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2022-07-19 16:25:48 -0400

    Revert "Simplify feedback form for testers and users"
    This reverts commit 8fef37529f57d68c3897943c2433be0e1213831b.

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.GithubFeedbackForm',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.ALToolbox'],
+      install_requires=[],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/GithubFeedbackForm/', package='docassemble.GithubFeedbackForm'),
      )
