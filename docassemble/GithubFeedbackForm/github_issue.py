67078e57c744e5cb2ef96caa064fd09d254ee21d
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2022-07-19 13:30:05 -0400

    Saves session ids from feedback on the server itself
    Allows admin users to examine user sessions in more detail to reproduce bugs and
    strange behavior.
    
    It stores the information in a DARedis database, and also adds a separate
    admin-only interview to directly jump to the session in question
    
    Can also save information on the server even if github isn't set up (though not
    optimized for that use yet).

--- docassemble/GithubFeedbackForm/github_issue.py
+++ docassemble/GithubFeedbackForm/github_issue.py
@@ -1,16 +1,20 @@
-import requests
-import json
 import importlib
-from docassemble.base.util import log, get_config, interview_url
+import json
+import requests
+import uuid
+from typing import Optional
+from docassemble.base.util import log, get_config, interview_url, DARedis
 
 # reference: https://gist.github.com/JeffPaine/3145490
 # https://docs.github.com/en/free-pro-team@latest/rest/reference/issues#create-an-issue
 
 # Authentication for user filing issue (must have read/write access to
 # repository to add issue to)
-__all__ = ['valid_github_issue_config', 'make_github_issue', 'feedback_link']
+__all__ = ['valid_github_issue_config', 'make_github_issue', 'save_session_info', 'set_session_github_url', 'feedback_link', 'redis_feedback_key']
 USERNAME = get_config('github issues',{}).get('username')
 TOKEN = get_config('github issues',{}).get('token')
+
+redis_feedback_key = 'docassemble-GithubFeedbackForm:feedback_info'
 
 def valid_github_issue_config():
   return bool(TOKEN)
@@ -21,8 +25,8 @@ def feedback_link(user_info_object=None,
                    github_user:str=None, 
                    variable:str=None, 
                    question_id:str=None,
-                   package_version=None,
-                   filename=None)->str:
+                   package_version:str=None,
+                   filename:str=None)->str:
   """
   Helper function to get a link to the GitHub feedback form.
   For simple usage, it should be enough to call
@@ -43,9 +47,10 @@ def feedback_link(user_info_object=None,
     try:
       _package_version = str(importlib.import_module(user_info_object.package).__version__)
     except:
-      _package_version = "playground"    
+      _package_version = "playground"
     if get_config('github issues',{}).get('default repository owner'):
       _github_user = get_config('github issues',{}).get('default repository owner')
+    _session_id = user_info_object.session
   
   # Allow keyword params to override any info from the user_info() object
   # We will try pulling the repo owner name from the Docassemble config
@@ -57,7 +62,7 @@ def feedback_link(user_info_object=None,
     _github_repo = github_repo
   else:
     _github_repo = "demo"
-    _github_user = "suffolklitlab-issues"    
+    _github_user = "suffolklitlab-issues"
   if variable:
     _variable = variable
   if question_id:
@@ -69,7 +74,7 @@ def feedback_link(user_info_object=None,
   
   if not i:
     i = "docassemble.GithubFeedbackForm:feedback.yml"
-    
+
   return interview_url(i=i, 
     github_repo=_github_repo, 
     github_user=_github_user, 
@@ -77,14 +82,52 @@ def feedback_link(user_info_object=None,
     question_id=_question_id, 
     package_version=_package_version, 
     filename=_filename,
-    local=False,reset=1)                         
+    session_id=_session_id,
+    local=False,reset=1)
 
-def make_github_issue(repo_owner, repo_name, template=None, title=None, body=None):
+def save_session_info(interview=None, session_id=None, template=None, body=None) -> Optional[dict]:
+    """Saves session information along with the feedback in a redis DB"""
+    if template:
+      body = template.content
+
+    if interview and session_id:
+      red = DARedis()
+      guid_map = red.get_data(redis_feedback_key)
+      if not guid_map:
+        guid_map = {}
+
+      uuid_for_session = str(uuid.uuid4())
+      body += f'\nUID for Session: {uuid_for_session}'
+      guid_map[uuid_for_session] = {'interview': interview, 'session_id': session_id, 'body': body}
+      red.set_data(redis_feedback_key, guid_map)
+      return uuid_for_session
+    else: # can happen if the forwarding interview didn't pass session info
+      return None
+
+def set_session_github_url(uuid_for_session:str, github_url:str) -> bool:
+    """Returns true if save was successful"""
+    red = DARedis()
+    guid_map = red.get_data(redis_feedback_key)
+    if not guid_map:
+      guid_map = {}
+
+    if uuid_for_session in guid_map:
+      guid_map[uuid_for_session]['html_url'] = github_url
+      red.set_data(redis_feedback_key, guid_map)
+      return True
+    else:
+      log(f'Cannot find {uuid_for_session} in redis DB')
+      return False
+
+def make_github_issue(repo_owner, repo_name, template=None, title=None, body=None) -> Optional[str]:
     """
     Create a new Github issue and return the URL.
+
+    template - the docassemble template for the github issue. Overrides `title` and `body` if provided.
+    title - the title for the github issue
+    body - the body of the github issue
     """
-    url = 'https://api.github.com/repos/%s/%s/issues' % (repo_owner, repo_name)
-    # log(url)
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/issues"
     # Headers
     if not TOKEN:
       log("Error creating issues: No valid GitHub token provided.")
