6a2b180a77c8ff84cc6a4f31a2e975c73786d1d3
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2023-10-16 11:57:58 -0400

    Get a default empty string if no DB_PASS

--- docassemble/GithubFeedbackForm/alembic/env.py
+++ docassemble/GithubFeedbackForm/alembic/env.py
@@ -12,7 +12,7 @@ from alembic import context
 config = context.config
 
 section = config.config_ini_section
-config.set_section_option(section, "DB_PASS", os.environ.get("DB_PASS"))
+config.set_section_option(section, "DB_PASS", os.getenv("DB_PASS", ""))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
