c9648da2d90edb599fe3d26eb085961edf8027c9
Bryce Willey <bryce.willey@suffolk.edu>
2025-07-22 12:20:45 -0400

    Added unit test to GitFeedbackForm and good/bad
    Mocks out the alchemy url function to point to a new postgres test container.
    
    Also switched to using sql alchemy 2.0 declarative base function,
    not the deprecated one.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -51,8 +51,7 @@ setup(name='docassemble.GithubFeedbackForm',
       author_email='qsteenhuis@suffolk.edu',
       license='The MIT License (MIT)',
       url='https://courtformsonline.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=['docassemble.ALToolbox>=0.6.0', 'google-generativeai'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/GithubFeedbackForm/', package='docassemble.GithubFeedbackForm'),
